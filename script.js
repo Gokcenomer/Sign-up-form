caf89b821d50b611b843349362ec62f07df3f2cd
Gokcenomer <b.ylmz55@hotmail.com>
2022-08-28 10:10:36 -0400

    Completed project

---
+++ script.js
@@ -0,0 +1,7 @@
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirmPassword');
+
+const submitButton = document.querySelector('button[type="submit"]');
+
+
+
